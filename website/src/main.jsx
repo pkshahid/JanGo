20c7fef7fb0e4a933be0528c83980c00ceaec55e
ntoric <ntoric.com@gmail.com>
2026-06-26 16:07:30 +0530

    Website Fix

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -4,6 +4,15 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister())
+  })
+  caches.keys().then((keys) => {
+    keys.forEach((key) => caches.delete(key))
+  })
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
